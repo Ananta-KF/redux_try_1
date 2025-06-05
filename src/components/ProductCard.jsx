ac7d63ac583e0080fc351c4ed7fd64da484a5584
Ananta Manoranjan <ananta.manoranjan@kornferry.com>
2025-06-05 12:47:03 +0530

    small changes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ export default function ProductCard({ product }) {
     <div className="card">
       <img src={product.image} alt={product.name}/>
       <h4>{product.name}</h4>
-      <p>₹{product.price.toFixed(2)}</p>
+      <p>Rs{product.price.toFixed(2)}</p>
       <Link to={`/product/${product.id}`}>View</Link>
     </div>
   );
